dce3086717a30d6b9a88da1c10a92b17c6526e0f
Vcky4 <okon.victor.u@gmail.com>
2022-03-10 13:44:29 -0800

    hello c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Enty point 
+ *
+ * Return always (0) success
+ */
+int main(void){
+	put("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
