e9a0d63d8cd27d1895ff3c0959d20b968121f5a8
Vcky4 <okon.victor.u@gmail.com>
2023-02-22 13:09:12 -0800

    fix print sign of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
         if (n > 0)
 	{
-		printf("+");
+		_putchar(43 + 0);
 		return(1);
 	}
 	else if(n == 0)
 	{
-		printf("0");
+		_putchar(0 + 48);
 		return(0);
 	}
 	else 
 	{
-		printf("-");
+		_putchar(45 + 0);
 		return(-1);
 	}
 }
