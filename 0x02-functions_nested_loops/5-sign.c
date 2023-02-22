1fd7fa2875c02c8705a2dbb7fe15b433e33a7c39
Vcky4 <okon.victor.u@gmail.com>
2023-02-22 13:11:27 -0800

    fix print sign of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - check for sign of number
  * @n: input.
- *
+ * 
  * Return: 1 and print + if n is greater zero
  * Return 0 and print 0 if n is zero
  * Return -1 and print - if n is less than zero
@@ -10,7 +10,7 @@
 
 int print_sign(int n)
 {
-        if (n > 0)
+        if(n > 0)
 	{
 		_putchar(43 + 0);
 		return(1);
